af0329c283423c925119745bc4f96481ce770043
nachoshei <udjnacho@gmail.com>
2024-04-24 09:17:02 -0300

    update

---
+++ Python/ejercicio7.py
@@ -0,0 +1,9 @@
+print("----------------------------------------------------")
+print("Ejercicio7: PROMEDIO DE DOS NUMEROS")
+print("----------------------------------------------------")
+A = int(input("Ingrese la primer nota: "))
+B = int(input("Ingrese la segunda nota: "))
+C = A + B
+D = C / 2
+print("El promedio es " , D)
+
