5bce5b487aecd984a6ec747aff843208ecc86755
nachoshei <udjnacho@gmail.com>
2024-04-25 11:54:20 -0300

    ....

---
+++ Python/Complementarios/comple10.py
@@ -0,0 +1,11 @@
+print("------------------------------------------------")
+print("Ejercicio10: DISTANCIA")
+print("------------------------------------------------")
+x1 = float(input("Ingrese el valor del punto x1: "))
+y1 = float(input("Ingrese el valor del punto y1: "))
+z1 = float(input("Ingrese el valor del punto z1: "))
+print("------------------------------------------------")
+x2 = float(input("Ingrese el valor del punto x2: "))
+y2 = float(input("Ingrese el valor del punto y2: "))
+z2 = float(input("Ingrese el valor del punto z2: "))
+dis =
---
+++ Python/Complementarios/comple9.py
@@ -0,0 +1,9 @@
+print("------------------------------------------------")
+print("Ejercicio9: GRADOS")
+print("------------------------------------------------")
+import math
+x = float(input("Ingrese angulo en radianes: "))
+sex = x*180/math.pi
+cen = x*200/math.pi
+print("En sexagesimales: " , sex)
+print("En centesimales: " , cen)
