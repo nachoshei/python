ee648b80a83806c584c48de02224de1e7d60532f
nachoshei <udjnacho@gmail.com>
2024-04-25 09:13:47 -0300

    modificaciones

---
+++ Python/Complementarios/comple4.py
@@ -0,0 +1,7 @@
+print("------------------------------------------------")
+print("Ejercicio4: VELOCIDAD")
+print("------------------------------------------------")
+tiempo = float(input("Ingrese el tiempo en segundos: "))
+distancia = float(input("Ingrese la distancia en metros: "))
+velocidad = distancia/tiempo
+print("La velocidad es: " , velocidad, "m/s")
