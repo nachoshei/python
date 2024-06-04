a5c2b062bace197fd9df7d607bc729f58d88a41c
nacho <udjnacho@gmail.com>
2024-06-04 11:42:44 +0200

    new commit

--- Python/Estructuras Repetitivas/Ejercicio2.py
+++ Python/Estructuras Repetitivas/Ejercicio2.py
@@ -1,14 +1,15 @@
 print("-----------------------------------------------")
 print("Ejercicio2: DIVISORES")
 print("-----------------------------------------------")
-
+i = 1
 print("Introduce un numero y para acabar uno negativo: ")
 num = int(input("Num: "))
 while num > 0:
     suma = 0
-    for i in range(1,num + 1):
+    for i in range(1,num+1):
         if num % i == 0:
             suma = suma + i
-print("La suma de los divisores del numero es: " , num," es:", suma)
-print("Introduce un numero y para acabar uno negativo: ")
-int(input("Num: "))
+#suma = suma + num
+    print("La suma de los divisores del numero:", num, "es", suma)
+    print("Introduce un numero y para acabar uno negativo: ")
+    num = int(input("Num: "))
--- Python/Estructuras Repetitivas/Ejercicio3.py
+++ Python/Estructuras Repetitivas/Ejercicio3.py
@@ -1,11 +1,19 @@
 print("-----------------------------------------------")
 print("Ejercicio3: ")
 print("-----------------------------------------------")
-primero = 1
+primero = 2
 limite = 1000
 cont = 0
-for i in range(limite):
+for i in range(primero, limite):
     primo = True
     j = 2
-    while (i > j) and (primo = True):
-        if
+    while (i > j) and (primo == True):
+        if i%j == 0:
+            primo = False
+            break
+        else:
+            j = j + 1
+    if primo == True:
+        print(i, "es primo.")
+        cont = cont + 1
+print("Entre", primero, "Y", limite, "Hay", cont, "nº primos")
