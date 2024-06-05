82e0cc999e5ccb1715960b3948b6a82009e89414
nachoshei <udjnacho@gmail.com>
2024-06-05 08:57:54 -0300

    Update Ejercicio1.py

--- Python/Estructuras Repetitivas/Ejercicio1.py
+++ Python/Estructuras Repetitivas/Ejercicio1.py
@@ -1,7 +1,6 @@
 print("-----------------------------------------------")
 print("Ejercicio1: INTERES")
 print("-----------------------------------------------")
-j = 1
 C = 0
 I = 0
 M = 0
@@ -9,6 +8,6 @@ while (C < 0) or (I <= 0) or (I >= 100) or (M <= 0):
     C = float(input("Ingrese el capital: "))
     I = float(input("Ingrese el interes: "))
     M = int(input("Ingrese los años: "))
-for j in range(M):
+for i in range(M):
     C = C * (1 + I / 100)
-print("Tienes ", C , "soles")
+print("Tienes ", C , "pesos")
