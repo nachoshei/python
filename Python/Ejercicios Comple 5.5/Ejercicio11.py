b21b1ff233ccee368ad415eaa7c0a77ac34cb914
nachoshei <udjnacho@gmail.com>
2024-06-19 11:22:40 -0300

    Update Ejercicio11.py

--- Python/Ejercicios Comple 5.5/Ejercicio11.py
+++ Python/Ejercicios Comple 5.5/Ejercicio11.py
@@ -5,9 +5,9 @@ i = 1
 con = 0
 num = 1
 #while i <= 100:
-for num in range(1, 100):
+for num in range(100 + 1):
     #num = int(input("Ingrese un numero: "))
     if num % 2 == 0:
         con = con + 1
-    i = i + 1
+    #i = i + 1
 print("En 100 numeros enteros hay", con,"numeros pares")
