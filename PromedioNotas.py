2f02340b49c61a01230bf0b3dded63ad4ed19737
nachoshei <udjnacho@gmail.com>
2024-08-23 11:33:53 -0300

    Create PromedioNotas.py

---
+++ PromedioNotas.py
@@ -0,0 +1,14 @@
+def buscar(alumBuscado, alumnos, cantidadA):
+    indAlum = -1
+    indice = 0
+    while indice < cantidadA and indAlum == -1:
+        if alumnos[indice] == alumBuscado:
+            indAlum = indice
+
+def calcularPromedio(nota1, nota2, nota3):
+    prom3 = (nota1 + nota2 + nota3)/3
+
+def promedioEscolar():
+    cantidadA = int(input("Ingrese la cantidad de alumnos: "))
+    for indice in range(cantidadA):
+        alumnos[indice] = int(input(""))
